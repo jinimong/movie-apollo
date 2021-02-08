25cc2cbb6a043b1a1c8878781d51db3ad49da85c
jwkim <phoongdeng@gmail.com>
2021-02-09 01:35:23 +0900

    Add detail page

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,43 @@
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import { gql, useQuery } from '@apollo/client';
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  width: 50%;
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const SubTitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-image: url(${(props) => props.bg});
+  background-size: cover;
+  background-position: center center;
+`;
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -21,11 +59,21 @@ const Detail = () => {
   if (error) {
     return <dir>Error : {error}</dir>;
   }
+  if (loading || !data) {
+    return <div>Loading ...</div>;
+  }
+  const { movie } = data;
   return (
-    <div>
-      {loading && <div>Loading ...</div>}
-      {!loading && data.movie && data.movie.title}
-    </div>
+    <Container>
+      <Column>
+        <Title>{movie.title}</Title>
+        <SubTitle>
+          {movie.language} : {movie.rating}
+        </SubTitle>
+        <Description>{movie.description_intro}</Description>
+      </Column>
+      <Poster bg={movie.medium_cover_image} />
+    </Container>
   );
 };
 export default Detail;
